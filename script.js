ad8c6a98ed35be024d6dde15d85a2dcbf4606a8f
theonlysamad7 <157276895+theonlysamad7@users.noreply.github.com>
2025-10-15 06:11:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -368,16 +368,3 @@ function renderLeaderboard() {
   });
 }
 
-/**********************
-EVENTS wiring
-**********************/
-document.addEventListener('DOMContentLoaded', () => {
-  if (dom.startBtn) dom.startBtn.addEventListener('click', startQuiz);
-  if (dom.nextBtn) dom.nextBtn.addEventListener('click', goNext);
-  if (dom.quitBtn) dom.quitBtn.addEventListener('click', quitToHome);
-  if (dom.restartBtn) dom.restartBtn.addEventListener('click', startQuiz);
-  if (dom.homeBtn) dom.homeBtn.addEventListener('click', showWelcome);
-
-  // show welcome leaderboard on first load
-  showWelcome();
-});
